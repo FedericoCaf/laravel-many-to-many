7aa2335966b19aebbd9e91e05f187aaf5a63ff40
FedericoCaf <fedetheshield09@gmail.com>
2022-02-07 17:53:09 +0100

    show-tag-ok

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Category;
 use App\Http\Controllers\Controller;
 use App\Post;
+use App\Tag;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
